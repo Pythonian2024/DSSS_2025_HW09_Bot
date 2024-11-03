e54254c06ce0d31a00f09b212e7c753277b2b9fb
Pythonian2024 <ab.alhaidari@yahoo.com>
2024-11-03 23:02:07 +0100

    inital setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 22:54:05 2024
+
+@author: abalh
+"""
+
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="Math Game package",
+    version="0.1.0",
+    description="A simple package for a Math Game",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DSSS_WS2024_Abdulaziz Alhaidari",
+    author_email="azez.alhaidari@fau.de",
+    url="https://github.com/Pythonian2024/DSSS-WS2024-HW02",  # optional: link to your GitHub repo
+    packages=find_packages(),
+    install_requires=[],  # dependencies (e.g., `random` is part of Python's standard library, so no extra needed here)
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
